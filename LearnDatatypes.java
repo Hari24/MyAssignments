7ca0f907d2f96cecc15dba2c224b9cec5394d330
Hari24 <Hari24@users.noreply.github.com>
2024-11-21 15:50:32 -0400

    Add files via upload

--- LearnDatatypes.java
+++ LearnDatatypes.java
@@ -3,6 +3,15 @@ package week1.day1;
 public class LearnDatatypes {
 
 	public static void main(String[] args) {
+		byte age=30;
+		int age1=40;
+		String str1="Test Leaf";
+		float rateOfInterest=3.95f;
+		long mobileNumber=9880490819l;
+		double amount=23.4567898;
+		float amount1 = 23.4567898f;
+		
+		System.out.println("Age = "+ age + " Age1= "+ age1 + " String = "+ str1 + " Long = "+  mobileNumber + " Float = "+ amount1 + " double = "+amount1);
 		
 
 	}
