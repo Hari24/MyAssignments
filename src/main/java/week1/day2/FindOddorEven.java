794837db266d73fc141ff3b49ef43d9229d0d173
Karanam <Karanam@KARANAM.home>
2024-11-22 15:37:13 -0400

    Initial code

---
+++ src/main/java/week1/day2/FindOddorEven.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class FindOddorEven {
+
+	public static void main(String[] args) {
+		int number1 = 10 , number2 = 21;
+		
+		if (number1 % 2 == 0) {
+			System.out.println(" Number " + number1 + " is an even number.");
+		}
+		
+		else {
+			System.out.println(" Number, " + number1 + " is a odd number.");
+		}
+		
+		if (number2 % 2 == 0) {
+			System.out.println(" Number, " + number2 + " is an even number.");
+		}
+		
+		else {
+			System.out.println(" Number " + number2 + " is a odd number.");
+		}
+	}
+
+}
